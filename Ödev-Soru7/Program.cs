3ae7318a77bef8fa7c21a84af65530601426b79d
Yusufkr03 <yusufkr2903@gmail.com>
2024-10-31 22:10:35 +0300

    Add files via upload

---
+++ Ödev-Soru7/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Çift mi Tek mi olduğunu öğrenmek istediğiniz sayıyıy giriniz");
+int sayi = int.Parse(Console.ReadLine());
+int kontrol = sayi % 2;
+if (kontrol == 0)
+{
+    Console.WriteLine("Girdiğiniz sayı çift");
+}
+else
+{
+    Console.WriteLine("Girdiğiniz Sayı Tek");
+}
+Console.ReadKey();
